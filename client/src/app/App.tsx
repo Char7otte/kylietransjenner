89ea957af755b505c754638eedeab06f8ceed7f7
shar <faethychan@gmail.com>
2025-12-01 06:17:10 +0800

    Display posts feature

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,24 +1,32 @@
+import { format } from "date-fns";
+
+import Posts from "../features/posts/components/posts.tsx";
+
+const posts = [
+    {
+        title: "Poopcap",
+        game: "Plants vs Zombies: Replanted",
+        date: format(new Date(2014, 1, 11), "yyyy-MM-dd"),
+    },
+    {
+        title: "the power of my femine curiosity",
+        game: "Bayonetta",
+        date: format(new Date(2015, 12, 21), "yyyy-MM-dd"),
+    },
+];
+
 function App() {
+    const date = new Date();
+
     return (
         <>
             <main>
-                <section id="preview">
-                    <h2>Preview</h2>
-                    <p>
-                        Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet suscipit sapien. Nulla iaculis eros a elit pharetra
-                        egestas.
-                    </p>
-                    <form>
-                        <input type="text" name="firstname" placeholder="First name" aria-label="First name" required />
-                        <input type="email" name="email" placeholder="Email address" aria-label="Email address" autoComplete="email" required />
-                        <button type="submit">Subscribe</button>
-                        <fieldset>
-                            <label htmlFor="terms">
-                                <input type="checkbox" role="switch" id="terms" name="terms" />I agree to the
-                                <a href="#">Privacy Policy</a>
-                            </label>
-                        </fieldset>
-                    </form>
+                <section>
+                    <h1 className={`text-center`}>Shar's Game Diary</h1>
+                    <p>Hello! It is {date.toString()}.</p>
+                </section>
+                <section>
+                    <Posts posts={posts} />
                 </section>
             </main>
         </>
