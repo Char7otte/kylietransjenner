a4f5df23e57a5b795c65d1204361dea22759b4b9
shar <faethychan@gmail.com>
2025-12-01 08:28:42 +0800

    Clean up files to test API fetching

--- client/src/features/posts/components/posts.tsx
+++ client/src/features/posts/components/posts.tsx
@@ -1,17 +1,33 @@
-import CreatePosts from "./create-posts";
-
 import { type Post } from "@/types/api";
 
-interface props {
-    posts: Post[];
-}
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-function Posts({ posts }: props) {
+function Posts() {
+    const queryClient = useQueryClient();
+
+    async function fetchPosts() {
+        const res = await fetch("/api/data");
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+
+    const { isPending, isError, data, error } = useQuery({ queryKey: ["posts"], queryFn: fetchPosts });
+    if (isPending) return <span>Loading...</span>;
+    if (isError) return <span>Error: {error.message}</span>;
+
     return (
         <>
-            {posts.map((post) => (
-                <CreatePosts post={post} />
-            ))}
+            {data.map((post: Post, index: number) => {
+                return (
+                    <article key={index}>
+                        <h2>Title:{post.title}</h2>
+                        <p>Game:{post.game}</p>
+                        <p>Date:{post.date}</p>
+                        <p>Content:{post.content}</p>
+                    </article>
+                );
+            })}
         </>
     );
 }
